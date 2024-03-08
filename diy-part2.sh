1d322265e788921f918ad63252ec6be3197b4f72
白 <39430235+3401797899@users.noreply.github.com>
2024-03-08 12:02:11 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,4 +11,5 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.6.1/192.168.1.1/g' package/base-files/files/bin/config_generate
+sed -i '238a\lsmod | grep -q mtkhnat && rmmod mtkhnat' ./feeds/luci/applications/luci-app-turboacc/root/etc/init.d/turboacc
