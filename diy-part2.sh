5e3d195a5453b77c1e9538b0b61ba7bb4c749fb2
白 <39430235+3401797899@users.noreply.github.com>
2024-03-10 12:53:02 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,6 +12,14 @@
 
 # Modify default IP
 sed -i 's/192.168.6.1/192.168.1.1/g' package/base-files/files/bin/config_generate
-sed -i '238a\lsmod | grep -q mtkhnat && rmmod mtkhnat' feeds/luci/applications/luci-app-turboacc/root/etc/init.d/turboacc
+# stop mtkhnat
+echo '#!/bin/sh /etc/rc.common
+
+START=90
+STOP=10
+
+start(){
+rmmod mtkhnat
+}' > package/base-files/files/etc/init.d/stop-mtkhnat
 # close ssr or openclash
 sed -i '202,206d' package/luci-theme-design/luasrc/view/themes/design/header.htm
